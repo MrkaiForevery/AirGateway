b6b859fad833eb84951755e43ef83a8861d621e2
liukai <17679237105@163.com>
2026-05-17 14:09:00 +0800

    测试一下

--- src/main/java/com/airfree/AirGatewayApplication.java
+++ src/main/java/com/airfree/AirGatewayApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+//这是一个启动类
 @EnableConfigurationProperties({
         AirRocketMQConfigProperties.class,
         AirRedissonConfigProperties.class,
